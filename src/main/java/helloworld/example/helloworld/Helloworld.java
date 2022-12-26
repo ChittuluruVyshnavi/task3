c74d6c8e6e587730bc81f229a814176055e384fe
Chittuluru Vyshnavi <m1082747@mindtree.com>
2022-12-26 09:28:35 +0530

    adding files

---
+++ src/main/java/helloworld/example/helloworld/Helloworld.java
@@ -0,0 +1,11 @@
+package helloworld.example.helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
